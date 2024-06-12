29b5073630c6005716f2ab8df18fef8a3d106924
LaitAh <lea.clair34@gmail.com>
2024-06-12 14:51:38 +0200

    setting up the second part of the page

--- www/wp-content/themes/leaclair/index.php
+++ www/wp-content/themes/leaclair/index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
 <?php get_template_part('templates/partone'); ?>
+<?php get_template_part('templates/parttwo'); ?>
 
 <?php get_footer(); ?>
---
+++ www/wp-content/themes/leaclair/templates/parttwo.php
@@ -0,0 +1,56 @@
+<!-- 2ème partie -->
+<div class="section-address">
+    <div class="page-section">
+        <div class="section-address--content">
+            <div class="section-address--title">
+                <div class="title-icon">
+                    <span class="material-symbols-outlined">confirmation_number</span>
+                </div>
+                <div class="title-text">
+                    <h2>Lorem ipsum dolor sit amet consectetur. Id turpis neque neque vel. Faucibus sit orci.</h2>
+                    <a href="#"><span class="material-symbols-outlined">arrow_forward</span></a>
+                </div>
+            </div>
+            <div class="section-address--image">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/le_corbusier-portrait-image.jpg" alt="Portrait de Le Corbusier">
+            </div>
+            <div class="section-address--agenda">
+                <div class="agenda-img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cite_fruges-image_agenda.jpg" alt="Détail d'une partie de la Cité Frugès">
+                </div>
+                <div class="agenda-content">
+                    <div class="agenda-content--date">
+                        <p class="date-icon"><span class="material-symbols-outlined">calendar_today</span></p>
+                        <p class="date-event">10 nov. 2022</p>
+                    </div>
+                    <div class="agenda-content--text">
+                        <p class="text-event--detail">Lorem ipsum dolor sit amet consectetur. Aenean a nullam.</p>
+                        <p class="text-event--infos">Tout public, gratuit</p>
+                    </div>
+                    <div class="agenda-content--button">
+                        <div class="button-left">
+                            <span class="material-symbols-outlined">fiber_manual_record</span>
+                            <span class="material-symbols-outlined">fiber_manual_record</span>
+                            <span class="material-symbols-outlined">fiber_manual_record</span>
+                        </div>
+                        <a href="#" class="button-right">Agenda <span class="material-symbols-outlined">arrow_forward</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="section-address--map">
+                <div class="map-img">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.107205960377!2d-0.6480862400955552!3d44.79900377757363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54d91a67fb3a7f%3A0x3b37b30fc18ab758!2zQ2l0w6kgRnJ1Z8OocywgMzM2MDAgUGVzc2Fj!5e0!3m2!1sfr!2sfr!4v1718113923243!5m2!1sfr!2sfr" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="map-infos">
+                    <p class="map-infos--icon"><span class="material-symbols-outlined">location_on</span></p>
+                    <h3 class="map-infos--title">Plan de la cité</h3>
+                    <h4 class="map-infos--address">
+                        2, rue Marc Sangnier<br/>
+                        33130 Bègles
+                    </h4>
+                    <p class="map-infos--opening">La cité est ouverte aujourd'hui <br/>de 10h à 13h et de 14h à 18h</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
