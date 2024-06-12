3d0b31a188af11ffa078d31dc029263d2d16ce1a
LaitAh <lea.clair34@gmail.com>
2024-06-12 14:55:56 +0200

    setting up the fourth part of the page

--- www/wp-content/themes/leaclair/index.php
+++ www/wp-content/themes/leaclair/index.php
@@ -3,5 +3,6 @@
 <?php get_template_part('templates/partone'); ?>
 <?php get_template_part('templates/parttwo'); ?>
 <?php get_template_part('templates/partthree'); ?>
+<?php get_template_part('templates/partfour'); ?>
 
 <?php get_footer(); ?>
---
+++ www/wp-content/themes/leaclair/templates/partfour.php
@@ -0,0 +1,81 @@
+<!-- 4ème partie -->
+<div class="section-librairy">
+    <div class="page-section">
+        <h2 class="section-librairy-title">
+            <span class="material-symbols-outlined">visibility</span>
+            Médiathèque
+        </h2>
+        <div class="section-library--content">
+            <div class="content-mosaic">
+                <div class="content-mosaic-element" id="element-1">
+                    <div class="element-title">
+                        <span class="element-title--icon material-symbols-outlined">draft</span>
+                        <h3 class="element-title--name">Titre du document</h3>
+                    </div>
+                    <div class="element-bottom">
+                        <h4 class="element-bottom-size">PDF (6 Mo)</h4>
+                        <a href="#" class="element-bottom--link">
+                            <span class="element-bottom--icon material-symbols-outlined">download</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-mosaic-element" id="element-2">
+                    <div class="element-title">
+                        <span class="element-title--icon material-symbols-outlined">mic</span>
+                        <h3 class="element-title--name">Podcast à découvrir sans plus attendre</h3>
+                    </div>
+                    <div class="element-bottom">
+                        <a href="#" class="element-bottom--link">
+                            <span class="material-symbols-outlined">arrow_forward</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-mosaic-element" id="element-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cite_fruges-image_agenda.jpg" alt="Photographie de la Cité Frugès" class="element-image">
+                </div>
+                <div class="content-mosaic-element" id="element-4">
+                    <div class="element-title">
+                        <span class="element-title--icon material-symbols-outlined">mic</span>
+                        <h3 class="element-title--name">Lorem ipsum dolor sit amet consectetur. Mauris scelerisque.</h3>
+                    </div>
+                    <div class="element-bottom">
+                        <a href="#" class="element-bottom--link">
+                            <span class="material-symbols-outlined">arrow_forward</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-mosaic-element" id="element-5">
+                    <div class="element-title">
+                        <span class="element-title--icon material-symbols-outlined">draft</span>
+                        <h3 class="element-title--name">Document complet à télécharger</h3>
+                    </div>
+                    <div class="element-bottom">
+                        <h4 class="element-bottom-size">PDF (11.9 Mo)</h4>
+                        <a href="#" class="element-bottom--link">
+                            <span class="element-bottom--icon material-symbols-outlined">download</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-mosaic-element" id="element-6">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cite_fruges-image_background_1.jpg" alt="Photographie de la Cité Frugès" class="element-image">
+                </div>
+                <div class="content-mosaic-element" id="element-7">
+                    <div class="element-title">
+                        <span class="element-title--icon material-symbols-outlined">draft</span>
+                        <h3 class="element-title--name">Titre du document</h3>
+                    </div>
+                    <div class="element-bottom">
+                        <h4 class="element-bottom-size">PDF (19 Mo)</h4>
+                        <a href="#" class="element-bottom--link">
+                            <span class="element-bottom--icon material-symbols-outlined">download</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-mosaic-element" id="element-8">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/le_corbusier-portrait-image.jpg" alt="Portrait du Corbusier" class="element-image">
+                </div>
+            </div>
+        </div>
+        <a href="#" class="content-title">Accès à la médiathèque <span class="material-symbols-outlined">arrow_forward</span></a>
+    </div>
+</div>
